eb21aa255a240e4f9d7f21d5c09006c820a21705
William Weiskopf <worldwidewilly@comcast.net>
2012-05-05 22:39:51 -0600

    Rearranging menu and trying to vertically center everything

--- funtions.php
+++ funtions.php
@@ -1,3 +1,12 @@
 <?php
-
+  /***************
+  RUN after parent functions.php
+  ***************/
+  function dandr_post_parent_setup() {
+    /*
+    * Unregister sidebar for pages
+    */
+      unregister_sidebar( 'Sidebar' );
+  }
+  add_action( 'after_setup_theme', 'dandr_post_parent_setup', 11);
 ?>
