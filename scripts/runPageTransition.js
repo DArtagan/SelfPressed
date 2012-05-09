cb41a9b745a52204585191cc5ddfd8b6bf88fb03
William Weiskopf <worldwidewilly@comcast.net>
2012-05-09 01:31:21 -0600

    Make the page change it's own minor function

--- scripts/runPageTransition.js
+++ scripts/runPageTransition.js
@@ -2,11 +2,11 @@ $(function() {
   $("header #nav-main li > a").click(function(e) {
     e.preventDefault();
     var tempHref = this.getAttribute('href');
+    function nextPage() { window.location = tempHref; }
     if($('#wrap').hasClass('wrap-collapse')) {
-      $("#wrap").removeClass('wrap-collapse').delay(900);
+      $("#wrap").removeClass('wrap-collapse').delay(900).nextPage();
     } else {
-      $("#wrap").addClass("wrap-collapse").delay(900);
+      $("#wrap").addClass("wrap-collapse").delay(900).nextPage();
     }
-    window.location = tempHref;
   });
 });
