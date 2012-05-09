3d8d328a170799fd077a2b0ec9ac8bd7f1499284
William Weiskopf <worldwidewilly@comcast.net>
2012-05-09 01:05:00 -0600

    Trying to prevent page change

--- scripts/runPageTransition.js
+++ scripts/runPageTransition.js
@@ -1,5 +1,6 @@
 $(function() {
-  $("header #nav-main li > a").click(function() {
+  $("header #nav-main li > a").click(function(e) {
+    e.preventDefault();
     var tempHref = this.getAttribute('href');
     if($('#wrap').hasClass('wrap-collapse')) {
       $("#wrap").removeClass('wrap-collapse');
