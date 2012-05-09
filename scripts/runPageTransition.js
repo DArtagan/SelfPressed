67830abde8bdbcda8a66a63c9f5007717ae57bae
William Weiskopf <worldwidewilly@comcast.net>
2012-05-09 01:44:39 -0600

    Still trying

--- scripts/runPageTransition.js
+++ scripts/runPageTransition.js
@@ -2,6 +2,19 @@ $(function() {
   $("header #nav-main li > a").click(function(e) {
     e.preventDefault();
     var tempHref = this.getAttribute('href');
+    if($('#wrap').hasClass('wrap-collapse')) {
+      $("#wrap").removeClass('wrap-collapse');
+    } else {
+      $("#wrap").addClass("wrap-collapse");
+    }
+    setTimeout(function() { window.location = tempHref; }, 900);
+  });
+});
+
+/*$(function() {
+  $("header #nav-main li > a").click(function(e) {
+    e.preventDefault();
+    var tempHref = this.getAttribute('href');
     var nextPage = function(x) { window.location = x; };
     if($('#wrap').hasClass('wrap-collapse')) {
       $("#wrap").removeClass('wrap-collapse').delay(900).nextPage(tempHref);
@@ -9,4 +22,4 @@ $(function() {
       $("#wrap").addClass("wrap-collapse").delay(900).nextPage(tempHref);
     }
   });
-});
+});*/
