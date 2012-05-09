ad6ada7b6531287292f03ebb196005fd5bdb5b43
William Weiskopf <worldwidewilly@comcast.net>
2012-05-09 01:27:19 -0600

    Now just need to add a slight delay to the page change

--- scripts/runPageTransition.js
+++ scripts/runPageTransition.js
@@ -3,9 +3,9 @@ $(function() {
     e.preventDefault();
     var tempHref = this.getAttribute('href');
     if($('#wrap').hasClass('wrap-collapse')) {
-      $("#wrap").removeClass('wrap-collapse');
+      $("#wrap").removeClass('wrap-collapse').delay(900);
     } else {
-      $("#wrap").addClass("wrap-collapse");
+      $("#wrap").addClass("wrap-collapse").delay(900);
     }
     window.location = tempHref;
   });
