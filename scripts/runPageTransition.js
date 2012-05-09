da4fea9670555f5dc4381073d0b6c573bb46194d
William Weiskopf <worldwidewilly@comcast.net>
2012-05-09 01:52:02 -0600

    Turn down delay slightly

--- scripts/runPageTransition.js
+++ scripts/runPageTransition.js
@@ -7,19 +7,6 @@ $(function() {
     } else {
       $("#wrap").addClass("wrap-collapse");
     }
-    setTimeout(function() { window.location = tempHref; }, 900);
+    setTimeout(function() { window.location = tempHref; }, 700);
   });
 });
-
-/*$(function() {
-  $("header #nav-main li > a").click(function(e) {
-    e.preventDefault();
-    var tempHref = this.getAttribute('href');
-    var nextPage = function(x) { window.location = x; };
-    if($('#wrap').hasClass('wrap-collapse')) {
-      $("#wrap").removeClass('wrap-collapse').delay(900).nextPage(tempHref);
-    } else {
-      $("#wrap").addClass("wrap-collapse").delay(900).nextPage(tempHref);
-    }
-  });
-});*/
