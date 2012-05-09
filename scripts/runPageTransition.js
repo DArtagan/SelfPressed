1bce6aca2ca2bac5b94f7848b8b13c74a07cb35b
William Weiskopf <worldwidewilly@comcast.net>
2012-05-08 22:24:58 -0600

    External javascript file not working

--- scripts/runPageTransition.js
+++ scripts/runPageTransition.js
@@ -1,8 +1,10 @@
-$("#menu-primary-navigation > li > a").click(function() {
-  var tempHref = this.getAttribute('href');
-  if($('#wrap').hasClass('wrap-collapse')) {
-    $("#wrap").removeClass('wrap-collapse');
-  } else {
-    $("#wrap").addClass("wrap-collapse");
-  }
+$(function() {
+  $("#menu-primary-navigation > li > a").click(function() {
+    var tempHref = this.getAttribute('href');
+    if($('#wrap').hasClass('wrap-collapse')) {
+      $("#wrap").removeClass('wrap-collapse');
+    } else {
+      $("#wrap").addClass("wrap-collapse");
+    }
+  });
 });
