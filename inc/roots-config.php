96a22df815498d275a4589a97aa9d21936555f20
William Weiskopf <worldwidewilly@comcast.net>
2012-05-10 21:59:05 -0600

    Setup sidebar for posts pages

---
+++ inc/roots-config.php
@@ -0,0 +1,11 @@
+<?php
+
+define('ROOT_RELATIVE_URLS',   true);
+define('POST_EXCERPT_LENGTH',  40);
+define('BOOTSTRAP_RESPONSIVE', true);
+define('WRAP_CLASSES',         'container');
+define('CONTAINER_CLASSES',    'row');
+// define('MAIN_CLASSES',         'span8');
+define('SIDEBAR_CLASSES',      'span4');
+define('FULLWIDTH_CLASSES',    'span12');
+define('GOOGLE_ANALYTICS_ID',  '');
