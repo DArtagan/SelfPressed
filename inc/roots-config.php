5a2b3e3c098b75a482a0258cb595ecb0d9bdbeb8
William Weiskopf <worldwidewilly@comcast.net>
2012-05-10 22:10:41 -0600

    Nullify the main div's span-8 class

--- inc/roots-config.php
+++ inc/roots-config.php
@@ -5,7 +5,7 @@ define('POST_EXCERPT_LENGTH',  40);
 define('BOOTSTRAP_RESPONSIVE', true);
 define('WRAP_CLASSES',         'container');
 define('CONTAINER_CLASSES',    'row');
-define('MAIN_CLASSES',         '');
+define('MAIN_CLASSES',         'test');
 define('SIDEBAR_CLASSES',      'span4');
 define('FULLWIDTH_CLASSES',    'span12');
 define('GOOGLE_ANALYTICS_ID',  '');
