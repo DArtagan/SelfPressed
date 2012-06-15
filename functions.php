abab697e7f0122e1834a7220acdcbd91d9fa3210
William Weiskopf <worldwidewilly@comcast.net>
2012-06-14 20:53:34 -0600

    Adding theme options that Customizer can use

--- functions.php
+++ functions.php
@@ -4,10 +4,32 @@
 * Custom Backgrounds
 */
   $defaults = array(
-    'default-color'          => 'ffffff',
+    'default-color' => 'ffffff',
   );
   add_theme_support( 'custom-background', $defaults );
 
+/*
+* Theme color support
+*/
+  $wp_customize->add_section( 'SelfPressed_color_scheme', array(
+    'title' => __( 'Color Scheme', 'SelfPressed' ),
+    'priority' => 35,
+  ) );
+
+  $wp_customize->add_setting( 'SelfPressed_theme_options[color_scheme]', array(
+    'default'        => '',
+    'type'           => 'option',
+    'capability'     => 'edit_theme_options',
+  ) );
+
+
+function themename_customize_register($wp_customize) {
+  // ... do stuff ...
+
+  function SelfPressed_customize_register($wp_customize) {
+    
+  }
+  add_action( 'customize_register', 'themename_customize_register' );
 
 
 /***************
